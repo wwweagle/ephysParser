b30552a6e384ca3902f2ecd9567cf4bfce529c0d
zx <zhangxiaoxing@ion.ac.cn>
2017-01-16 23:43:46 +0800

    plot traces. p value routines saved in comments.

--- alignLickTrigAvg.m
+++ alignLickTrigAvg.m
@@ -1,7 +1,8 @@
-function p=alignLickTrigAvg(keyIdx,lickTrigAvg)
+function [p,fr]=alignLickTrigAvg(keyIdx,lickTrigAvg)
 
 suIdx=1;
 p=[];
+fr=[];
 for i=1:size(keyIdx,1)
     fname=strtrim(keyIdx{i,1});
     equf=-1;
@@ -32,13 +33,29 @@ for i=1:size(keyIdx,1)
         %%%%%%%%%%%%%%%%%%%
         %%% su is found %%%
         %%%%%%%%%%%%%%%%%%%
-        befLick=lickTrigAvg{j,3}(l,:,1);
-        aftLick=lickTrigAvg{j,3}(l,:,2);
-        if isequal(sort(befLick),sort(aftLick))
-            p=[p;1,size(befLick,2),sum(befLick,2),sum(aftLick,2)];
-        else
-            p=[p;ranksum(double(befLick),double(aftLick)),size(befLick,2),sum(befLick,2),sum(aftLick,2)];
-        end
+%         befLick=lickTrigAvg{j,3}(l,:,1);
+%         aftLick=lickTrigAvg{j,3}(l,:,2);
+%         if isequal(sort(befLick),sort(aftLick))
+%             p=[p;1,size(befLick,2),sum(befLick,2),sum(aftLick,2)];
+%         else
+%             p=[p;ranksum(double(befLick),double(aftLick)),size(befLick,2),sum(befLick,2),sum(aftLick,2)];
+%         end
+            licks=size(lickTrigAvg{j,3}{l,1},1);
+            concated=[];
+            bf=[];
+            aft=[];
+            for m=1:licks
+                if ~isempty(lickTrigAvg{j,3}{l,1})
+                    concated=[concated,lickTrigAvg{j,3}{l,1}{m,1}];
+                    bf=[bf;sum(lickTrigAvg{j,3}{l,1}{m,1}>-0.25 & lickTrigAvg{j,3}{l,1}{m,1}<0)];
+                    aft=[aft;sum(lickTrigAvg{j,3}{l,1}{m,1}>0 & lickTrigAvg{j,3}{l,1}{m,1}<0.25)];
+                else
+                    bf=[bf;0];
+                    aft=[aft;0];
+                end
+            end
+            p=[p;ranksum(bf,aft)];
+            fr=[fr;(histcounts(concated,100))./licks/0.02];
     end
 end
 end
--- sel2wayDNMS.m
+++ sel2wayDNMS.m
@@ -22,52 +22,31 @@ for f=1:length(selA)
     currSU=currSU+SUCount;
 end
 
-% currSU=0;
-% for f=1:length(selGo13A)
-%     SUCount=size(selGo13A{f},1);
-%     for SU=1:SUCount
-%         for bin=1:16/binSize
-%             s13(SU+currSU,bin)=ranksum([flatten(selGo13A{f},SU,bin+1/binSize);flatten(selNogo13A{f},SU,bin+1/binSize)],[flatten(selGo13B{f},SU,bin+1/binSize);flatten(selNogo13B{f},SU,bin+1/binSize)]);
-%             d13(SU+currSU,bin)=ranksum([flatten(selGo13A{f},SU,bin+1/binSize);flatten(selGo13B{f},SU,bin+1/binSize)],[flatten(selNogo13A{f},SU,bin+1/binSize);flatten(selNogo13B{f},SU,bin+1/binSize)]);
-%         end
-%     end
-%     currSU=currSU+SUCount;
-% end
-
-
-%     figure();
-%     sSel=(sample<0.05);
-%     dSel=(distractor<0.05)*2;
-%     sel8=sSel+dSel;
-%     selStack=[sum(sel8==1)',sum(sel8==3)',sum(sel8==2)']./size(sSel,1);
-%     bar(selStack,'stacked');
-% 
-%     figure();
-%     sSel13=(s13<0.05);
-%     dSel13=(d13<0.05)*2;
-%     sel13=sSel13+dSel13;
-%     selStack=[sum(sel13==1)',sum(sel13==3)',sum(sel13==2)']./size(sSel,1);
-%     bar(selStack,'stacked');
 
 % s=combine8_13(sample,s13)<0.05;
 % d=(combine8_13(distractor,d13)<0.05)*2;
-smp=double(sample<0.05);
-tst=(double(test)<0.05)*2;
-mch=(double(match)<0.05)*4;
-
-% smp=smp+tst;
+if exist('subgrp','var')
+    smp=double(sample(subgrp,:)<0.05);
+    tst=(double(test(subgrp,:))<0.05)*2;
+    mch=(double(match(subgrp,:))<0.05)*4;
+else
+    smp=double(sample<0.05);
+    tst=(double(test)<0.05)*2;
+    mch=(double(match)<0.05)*4;
+end
+smp=smp+tst+mch;
 % selStack=[sum(sel==1)',sum(sel==3)',sum(sel==2)']./size(sel,1);
-selStack=[sum(smp==1)]./size(smp,1);
-% selStack=[sum(smp==1)',sum(smp==2)',sum(smp==3)',sum(smp==4)',sum(smp==5)',sum(smp==6)',sum(smp==7)']./size(smp,1);
+% selStack=[sum(smp==1)]./size(smp,1);
+selStack=[sum(smp==1)',sum(smp==2)',sum(smp==3)',sum(smp==4)',sum(smp==5)',sum(smp==6)',sum(smp==7)']./size(smp,1);
 figure('Color','w','Position',[100,100,350,250]);
 subplot('Position',[0.15,0.15,0.8,0.55]);
 bh=bar(selStack,'stacked');
 % ph=plot(selStack,'LineWidth',2);
 % bh(1).FaceColor='w';
 % bh(2).FaceColor=[0.8,0.8,0.8];
-bh(1).FaceColor='k';
+% bh(1).FaceColor='k';
 % xlim([0,(delay+5)/binSize+0.5]);
-xlim([0,(delay+3)/binSize+0.5]);
+xlim([0,(delay+6)/binSize+0.5]);
 % set(gca,'XTick',[2.5,4.5,12.5,14.5,16.5,17.5],'TickDir','out','XTickLabel',[]);%,'YTick',0:0.4:0.8);
 yspan=ylim();
 line(repmat(dashes,2,1),repmat(yspan()',1,length(dashes)),'LineStyle',':','Color','k');
@@ -77,16 +56,11 @@ xlabel('Time (s)');
 ylim(yspan);
 % legend({'Sample','Mixed','Match/Non-match'});
 % legend({'Sample'});
-% legend({'Sample','Test','Sample & Test','Match - NonMatch','Sample & Match','Test & Match','Sample, Test & Match'});
+legend({'Sample','Test','Sample & Test','Match - NonMatch','Sample & Match','Test & Match','Sample, Test & Match'});
 disp(chi2(mergeBin(smp)));
 
     function out=mergeBin(in)
-%         out=nan(size(in,1),size(in,2)/(1/binSize));
-%         n=1/binSize;
-%         for i=1:n:size(in,2)
-%             out(:,(i+1)/2)=sum(in(:,[i,i+1]),2);
-%         end
-out=reshape(in,[],size(in,2)/(1/binSize));
+        out=reshape(in,[],size(in,2)/(1/binSize));
     end
 
     function out=chi2(in)
